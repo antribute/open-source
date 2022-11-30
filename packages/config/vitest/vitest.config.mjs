d45ec80d98cc2417c2b600228df13405b2ea7fed
James Kienle <james.kienle@gmail.com>
2022-11-30 13:19:34 -0800

    Fix bad coverage numbers lol

--- packages/config/vitest/vitest.config.mjs
+++ packages/config/vitest/vitest.config.mjs
@@ -7,7 +7,7 @@ export default defineConfig({
     coverage: {
       all: true,
       enabled: true,
-      exclude: ['index.ts'],
+      exclude: ['index.ts', 'index.tsx', '**/*.stories.tsx'],
       provider: 'c8',
       reporter: ['text', 'cobertura'],
       reportsDirectory: './coverage',
