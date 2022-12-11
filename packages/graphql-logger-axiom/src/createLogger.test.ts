66b64e81f169f59f294323d9e409ce8f940496f0
James Kienle <james.kienle@gmail.com>
2022-12-11 09:18:47 -0800

    Add 100% code coverage to this bitch

--- packages/graphql-logger-axiom/src/createLogger.test.ts
+++ packages/graphql-logger-axiom/src/createLogger.test.ts
@@ -1,5 +1,96 @@
-import { describe, it } from 'vitest';
+import Axiom from '@axiomhq/axiom-node';
+import { afterAll, beforeEach, describe, expect, it, vi } from 'vitest';
+import createLogger, { createLoggerFunc, LoggerTypes } from './createLogger';
 
 describe('createLogger', () => {
-  it.todo('should have tests');
+  vi.mock('@axiomhq/axiom-node', () => ({
+    default: vi.fn(),
+  }));
+
+  it('should create an Axiom client and 4 loggers', () => {
+    const logger = createLogger({ axiomOrgId: 'foo', axiomToken: 'bar', dataset: 'baz' });
+    expect(logger).toHaveProperty('debug');
+    expect(logger).toHaveProperty('info');
+    expect(logger).toHaveProperty('warn');
+    expect(logger).toHaveProperty('error');
+  });
+});
+
+describe('createLoggerFunc', () => {
+  const consoleDebugSpy = vi.spyOn(console, 'debug');
+  const consoleInfoSpy = vi.spyOn(console, 'info');
+  const consoleWarnSpy = vi.spyOn(console, 'warn');
+  const consoleErrorSpy = vi.spyOn(console, 'error');
+  const consoleLogSpy = vi.spyOn(console, 'log');
+
+  const mockIngestEvents = vi.fn().mockImplementation(() => Promise.resolve());
+  const mockAxiom = {
+    datasets: {
+      ingestEvents: mockIngestEvents,
+    },
+  } as unknown as Axiom;
+
+  const MOCK_DATASET = 'foo';
+
+  beforeEach(() => {
+    vi.resetAllMocks();
+  });
+
+  afterAll(() => {
+    vi.restoreAllMocks();
+  });
+
+  it('should run console.debug when level is debug', () => {
+    const LoggerFunc = createLoggerFunc(mockAxiom, MOCK_DATASET, LoggerTypes.debug);
+    LoggerFunc({ message: 'bar' });
+    expect(consoleDebugSpy).toHaveBeenCalledTimes(1);
+    expect(mockIngestEvents).toHaveBeenCalledTimes(1);
+  });
+
+  it('should run console.info when level is info', () => {
+    const LoggerFunc = createLoggerFunc(mockAxiom, MOCK_DATASET, LoggerTypes.info);
+    LoggerFunc({ message: 'bar' });
+    expect(consoleInfoSpy).toHaveBeenCalledTimes(1);
+    expect(mockIngestEvents).toHaveBeenCalledTimes(1);
+  });
+
+  it('should run console.warn when level is warn', () => {
+    const LoggerFunc = createLoggerFunc(mockAxiom, MOCK_DATASET, LoggerTypes.warn);
+    LoggerFunc({ message: 'bar' });
+    expect(consoleWarnSpy).toHaveBeenCalledTimes(1);
+    expect(mockIngestEvents).toHaveBeenCalledTimes(1);
+  });
+
+  it('should run console.error when level is error', () => {
+    const LoggerFunc = createLoggerFunc(mockAxiom, MOCK_DATASET, LoggerTypes.error);
+    LoggerFunc({ message: 'bar' });
+    expect(consoleErrorSpy).toHaveBeenCalledTimes(1);
+    expect(mockIngestEvents).toHaveBeenCalledTimes(1);
+  });
+
+  it('should run console.log when level is invalid', () => {
+    // @ts-expect-error: This will error since this is testing a non-ts case
+    const LoggerFunc = createLoggerFunc(mockAxiom, MOCK_DATASET, null);
+    LoggerFunc({ message: 'bar' });
+    expect(consoleLogSpy).toHaveBeenCalledTimes(1);
+    expect(mockIngestEvents).toHaveBeenCalledTimes(1);
+  });
+
+  it('should send an object with a message key to axiom if a string is provided as the function arg', () => {
+    const LoggerFunc = createLoggerFunc(mockAxiom, MOCK_DATASET, LoggerTypes.debug);
+    LoggerFunc('this is a big test');
+    expect(mockIngestEvents).toHaveBeenCalledWith(MOCK_DATASET, {
+      level: LoggerTypes.debug,
+      message: 'this is a big test',
+    });
+  });
+
+  it('should gracefully handle failures in Axiom', () => {
+    mockIngestEvents.mockRejectedValue(new Error('This is an error'));
+    const LoggerFunc = createLoggerFunc(mockAxiom, MOCK_DATASET, LoggerTypes.debug);
+    LoggerFunc({ message: 'bar' });
+    // We're making sure console.debug still runs here to ensure this isn't a false positive
+    expect(consoleDebugSpy).toHaveBeenCalledTimes(1);
+    expect(consoleErrorSpy).toHaveBeenCalledTimes(1);
+  });
 });
--- packages/graphql-logger-axiom/src/createLogger.ts
+++ packages/graphql-logger-axiom/src/createLogger.ts
@@ -1,7 +1,7 @@
 import Axiom from '@axiomhq/axiom-node';
 import clc from 'cli-color';
 
-export type LoggerFunc = (log: Record<string, unknown>) => void;
+export type LoggerFunc = (log: Record<string, unknown> | string) => void;
 export enum LoggerTypes {
   debug = 'debug',
   info = 'info',
@@ -15,9 +15,9 @@ export interface CreateLoggerParams {
   dataset: string;
 }
 
-const createLoggerFunc =
+export const createLoggerFunc =
   (axiom: Axiom, dataSet: string, level: LoggerTypes): LoggerFunc =>
-  (args) => {
+  async (args) => {
     let logPrefix: string;
     let logFunc: (...fnargs: unknown[]) => void;
     switch (level) {
@@ -39,7 +39,6 @@ const createLoggerFunc =
         break;
       case LoggerTypes.error:
         // eslint-disable-next-line no-console
-        console.log('@@@@@@@@@@');
         logFunc = console.error;
         logPrefix = clc.red('[Error]: ');
         break;
@@ -49,7 +48,7 @@ const createLoggerFunc =
         logPrefix = '[Log]: ';
     }
 
-    if (args.message) {
+    if (typeof args !== 'string' && args.message) {
       logFunc(logPrefix, args.message, args);
     } else {
       logFunc(logPrefix, args);
@@ -60,14 +59,19 @@ const createLoggerFunc =
       finalizedArgs = { message: args };
     }
 
-    axiom.datasets.ingestEvents(dataSet, { ...finalizedArgs, level }).catch((err) => {
+    try {
+      await axiom.datasets.ingestEvents(dataSet, {
+        ...(finalizedArgs as Record<string, unknown>),
+        level,
+      });
+    } catch (err) {
       // Obviously we want this log to run, we need to know if there's something up with Axiom
       // eslint-disable-next-line no-console
       console.error(
         'There was an error sending logs to Axiom, this is really bad and should never happen',
         err
       );
-    });
+    }
   };
 
 const createLogger = ({
