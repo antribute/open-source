15b2a001a7ebc716b514d4ff1eb38d2dd5d4dcfe
James Kienle <james.kienle@gmail.com>
2022-12-11 17:39:22 -0800

    Finally fix the build system fuckery

--- apps/landing-page/src/pages/api/graphql.api.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, expect, it } from 'vitest';
-import handler from './graphql.api';
-
-describe('api/graphql', () => {
-  it.skip('should have a graphql endpoint that matches the route', () => {
-    expect(handler.graphqlEndpoint).toEqual('/api/graphql');
-  });
-});
