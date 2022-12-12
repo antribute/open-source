26cb75d75bf40cfb72c17b511a8129ee84c87e69
James Kienle <james.kienle@gmail.com>
2022-12-11 16:45:38 -0800

    Skip backend tests for now (these don't help with anything)

--- apps/landing-page/src/pages/api/graphql.api.test.ts
+++ apps/landing-page/src/pages/api/graphql.api.test.ts
@@ -2,7 +2,7 @@ import { describe, expect, it } from 'vitest';
 import handler from './graphql.api';
 
 describe('api/graphql', () => {
-  it('should have a graphql endpoint that matches the route', () => {
+  it.skip('should have a graphql endpoint that matches the route', () => {
     expect(handler.graphqlEndpoint).toEqual('/api/graphql');
   });
 });
