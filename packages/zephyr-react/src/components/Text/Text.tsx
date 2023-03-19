221dcfe2753579102130537b61d04b02fcced30d
Tyler Mitchell <tyler.davis.mitchell@gmail.com>
2023-03-19 17:40:54 -0500

    add balancer to `Text` component

--- packages/zephyr-react/src/components/Text/Text.tsx
+++ packages/zephyr-react/src/components/Text/Text.tsx
@@ -1,22 +1,36 @@
+import { lazy } from 'react';
 import { textVariants } from 'styles/text.variants';
-import { classed } from 'utils/classed';
+import { Classed, classed } from 'utils/classed';
 
-export const Text = classed('span', 'text-content dark:text-content-inverse', {
+const Balancer = lazy(() => import('react-wrap-balancer'));
+
+export const TextElement = classed('span', 'text-content dark:text-content-inverse', {
   variants: {
     size: textVariants.size,
     fontWeight: textVariants.fontWeight,
-    color: {
-      weak: 'text-content-weak dark:text-content-inverse-weak',
-      moderate: 'text-content-moderate dark:text-content-inverse-moderate',
-      high: 'text-content-high dark:text-content-inverse-high',
-      strong: 'text-content-intense dark:text-content-inverse-intense',
-    },
+    color: textVariants.color,
     block: {
       true: 'block',
     },
   },
-
   defaultVariants: {
     color: 'moderate',
   },
 });
+
+type TextProps = {
+  className?: string;
+  children?: React.ReactNode;
+  balancer?: boolean;
+  balancerRatio?: number;
+  as?: 'p' | 'span' | 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
+} & Classed.VariantProps<typeof TextElement>;
+
+export const Text = ({ children, className, balancerRatio, balancer, as, ...props }: TextProps) => {
+  const showBalancer = balancer ?? typeof balancerRatio === 'number';
+  return (
+    <TextElement className={className} as={as as any} {...props}>
+      {showBalancer ? <Balancer ratio={balancerRatio}>{children}</Balancer> : children}
+    </TextElement>
+  );
+};
--- packages/zephyr-react/src/styles/text.variants.ts
+++ packages/zephyr-react/src/styles/text.variants.ts
@@ -1,16 +1,30 @@
 import clsx from 'clsx';
 
 export const textVariants = {
+  variant: {
+    body: 'font-body',
+    heading: 'font-heading',
+    mono: 'font-mono',
+  },
   fontWeight: {
-    body: clsx('font-body'),
     medium: clsx('font-medium'),
     bold: clsx('font-bold'),
-    heading: clsx('font-heading'),
   },
   size: {
     xs: clsx('text-xs'),
     sm: clsx('text-sm'),
     md: clsx('text-md'),
     lg: clsx('text-lg'),
+  },
+  color: {
+    tint: clsx('text-content-tint'),
+    ghost: clsx('text-content-ghost'),
+    subtle: clsx('text-content-subtle'),
+    weak: clsx('text-content-weak'),
+    moderate: clsx('text-content-moderate'),
+    high: clsx('text-content-high'),
+    strong: clsx('text-content-intense'),
+    inverse: clsx('text-inverse'),
+    danger: clsx('text-danger'),
   },
 };
