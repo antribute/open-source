8712d681786ede95863b84299e12fa335a1773f2
James Kienle <james.kienle@gmail.com>
2023-03-26 12:53:17 -0700

    Nextjs platform generation

--- packages/backend-cli/src/commands/generate.ts
+++ packages/backend-cli/src/commands/generate.ts
@@ -1,12 +1,14 @@
 import { getConfig } from 'utils/config';
 import { generateGraphqlBuilder, generateGraphqlSchema } from 'utils/graphql';
 import { generateOrmConfig } from 'utils/orm';
+import { generatePlatform } from 'utils/platform';
 
 const generate = async (configPath = '.antributerc') => {
   const config = await getConfig(configPath);
   await generateOrmConfig(config);
   await generateGraphqlBuilder(config);
   await generateGraphqlSchema(config);
+  await generatePlatform(config);
 };
 
 export default generate;
