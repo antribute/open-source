95b0fd0c2bf9c88476803ef3dfa1f7f78c46d83e
Tyler Mitchell <tyler.davis.mitchell@gmail.com>
2023-03-28 18:20:42 -0500

    update eslint config

--- packages/config/eslint/eslint-base.cjs
+++ packages/config/eslint/eslint-base.cjs
@@ -15,15 +15,18 @@ module.exports = {
   rules: {
     'no-extra-boolean-cast': 'off',
     'no-param-reassign': 'off',
+    'no-underscore-dangle': 'off',
     'import/extensions': 'off',
     'import/prefer-default-export': 'off',
     'arrow-body-style': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-non-null-assertion': 'off',
     '@typescript-eslint/no-shadow': 'off',
     '@typescript-eslint/no-unsafe-call': 'off',
     '@typescript-eslint/no-unnecessary-type-assertion': 'off',
     '@typescript-eslint/no-unsafe-return': 'off',
     '@typescript-eslint/no-use-before-define': 'off',
+    '@typescript-eslint/prefer-reduce-type-parameter': 'off',
     // This sometimes interferes with third-party packages
     '@typescript-eslint/no-unsafe-member-access': 'off',
     // This allows us to import devdeps in tests
