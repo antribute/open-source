d37a5a9c5fdd24a02585014b9d8c256616dd7b4a
Tyler Mitchell <tyler.davis.mitchell@gmail.com>
2023-03-28 19:52:08 -0500

    fix linting errors

--- packages/zephyr-core/src/tailwindConfig.ts
+++ packages/zephyr-core/src/tailwindConfig.ts
@@ -110,14 +110,14 @@ const config = {
     divideOpacity: ({ theme }) => theme('borderOpacity'),
     divideWidth: ({ theme }) => theme('borderWidth'),
     dropShadow: {
-      // @ts-expect-error
+      // @ts-expect-error This is believed to be valid
       DEFAULT: ['0 1px 2px rgb(0 0 0 / 0.1)', '0 1px 1px rgb(0 0 0 / 0.06)'],
       sm: '0 1px 1px rgb(0 0 0 / 0.05)',
-      // @ts-expect-error
+      // @ts-expect-error This is believed to be valid
       md: ['0 4px 3px rgb(0 0 0 / 0.07)', '0 2px 2px rgb(0 0 0 / 0.06)'],
-      // @ts-expect-error
+      // @ts-expect-error This is believed to be valid
       lg: ['0 10px 8px rgb(0 0 0 / 0.04)', '0 4px 3px rgb(0 0 0 / 0.1)'],
-      // @ts-expect-error
+      // @ts-expect-error This is believed to be valid
       xl: ['0 20px 13px rgb(0 0 0 / 0.03)', '0 8px 5px rgb(0 0 0 / 0.08)'],
       '2xl': '0 25px 25px rgb(0 0 0 / 0.15)',
       none: '0 0 #0000',
