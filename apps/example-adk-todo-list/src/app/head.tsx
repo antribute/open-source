0304e4b674884ecb3fc85fcea7a00a9fe1e43a7c
James Kienle <james.kienle@gmail.com>
2023-04-27 08:31:20 -0700

    Add shell of example application

---
+++ apps/example-adk-todo-list/src/app/head.tsx
@@ -0,0 +1,10 @@
+function Head() {
+  return (
+    <>
+      <meta name="robots" content="index, follow" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+    </>
+  );
+}
+
+export default Head;
---
+++ apps/example-adk-todo-list/src/app/page.tsx
@@ -0,0 +1,9 @@
+function Page() {
+  return (
+    <main>
+      <h1>Hello, world!</h1>
+    </main>
+  );
+}
+
+export default Page;
