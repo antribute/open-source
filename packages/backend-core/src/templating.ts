3c74a35ad340d2da40913408cb32ad39543cccbf
James Kienle <james.kienle@gmail.com>
2023-04-27 10:55:38 -0700

    Fix Quotes Getting Escaped In Templating

--- packages/backend-core/src/templating.ts
+++ packages/backend-core/src/templating.ts
@@ -1,6 +1,6 @@
 import handlebars from 'handlebars';
 
 export const populateTemplate = <ParamType extends object>(template: string, params: ParamType) => {
-  const compiledTemplate = handlebars.compile(template);
+  const compiledTemplate = handlebars.compile(template, { noEscape: true });
   return compiledTemplate(params);
 };
