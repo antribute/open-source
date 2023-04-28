90825c94b2c08cf0ed5438a70c3122d7cf5a2e0d
James Kienle <james.kienle@gmail.com>
2023-04-28 11:44:19 -0700

    Clean Up Original Backend Work (ew)

--- packages/backend/src/commands/generate.ts
+++ packages/backend/src/commands/generate.ts
@@ -1,19 +1,12 @@
-// import { generateGraphqlBuilder, generateGraphqlSchema } from 'utils/graphql';
-// import { generatePlatform } from 'utils/platform';
-
 import { getConfig, importAndRunGenerator } from '@antribute/backend-core';
 
 const generate = async (configPath?: string) => {
   const config = await getConfig(configPath);
 
-  // The very first step is to generate our auth. The choices that the developer makes for auth can
-  // have a direct effect on other portions of the app such as the ORM or GraphQL Resolvers
   await importAndRunGenerator('Authorization', config.auth.platform, config);
   await importAndRunGenerator('ORM', config.orm.platform, config);
   await importAndRunGenerator('GraphQL', config.graphql.platform, config);
-  // await generateGraphqlBuilder(config);
-  // await generateGraphqlSchema(config);
-  // await generatePlatform(config);
+  await importAndRunGenerator('Server', config.server.platform, config);
 };
 
 export default generate;
