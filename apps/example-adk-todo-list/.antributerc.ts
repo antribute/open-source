fef179c401fe66edf1409d7842f8f8ddb9c1b5a8
James Kienle <james.kienle@gmail.com>
2023-10-09 11:02:44 -0600

    Removed deprecated ADK example (replace with TypeAPI when complete)

--- apps/example-adk-todo-list/.antributerc.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from '@antribute/backend-core';
-
-export default defineConfig({
-  auth: { customConfig: {}, platform: '@antribute/backend-auth-nextauth' },
-  logLevel: 'info',
-  permissions: { customConfig: {}, platform: '@antribute/backend-perms-auth0-fga' },
-  server: { customConfig: {}, platform: '@antribute/backend-server-nextjs' },
-});
--- apps/example-adk-todo-list/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-function Page() {
-  return (
-    <main>
-      <h1>Hello, world!</h1>
-    </main>
-  );
-}
-
-export default Page;
