142747186856f456608aa415714a1f9dc68f7e49
James Kienle <james.kienle@gmail.com>
2023-12-10 09:34:42 -0800

    Create PostgreSQL version of drizzle-crud

--- packages/drizzle-crud/README.md
+++ packages/drizzle-crud/README.md
@@ -8,11 +8,21 @@ Instant CRUD Methods for Drizzle Models (prototyping has never been faster!)
 bun add @antribute/drizzle-crud drizzle-orm
 ```
 
+## Warning
+
+Due to a lack of `.returning()` support in MySQL, this package does not support MySQL at this time.
+This is something we plan on adding in the near future, but as of right now we only support
+PostgreSQL and SQLite.
+
 ## Usage
 
 ```typescript
-import { createCrudMethods } from '@antribute/typebox-errors';
-import { pgEnum, pgTable, serial, text, timestamp } from 'drizzle-orm/<adapter>';
+// This example uses SQLite but works practically the same with Postgres
+
+import { createCrudMethods } from '@antribute/drizzle-crud/sqlite-core';
+import { Database } from 'bun:sqlite';
+import { eq } from 'drizzle-orm';
+import { drizzle } from 'drizzle-orm/bun-sqlite';
 
 const users = pgTable('users', {
   id: serial('id').primaryKey(),
@@ -22,6 +32,24 @@ const users = pgTable('users', {
   createdAt: timestamp('created_at').notNull().defaultNow(),
 });
 
-const { createOne, createMany, readOne, readMany, updateOne, updateMany, deleteOne, deleteMany } =
-  createCrudMethods(users, users.id, { pagination: 'cursor' });
+const sqlite = new Database(':memory:');
+const db = drizzle(sqlite);
+
+const usersMethods = createCrudMethods(db, users, users.id, { paginationMethod: 'limit-offset' });
+
+// userMethods.createMany() is also available for batch inserts
+const newUser = await usersMethods.createOne({
+  name: 'My Super Cool User',
+  email: 'someone@example.net',
+  role: 'user',
+});
+
+const user = await usersMethods.readOne(newUser.id);
+const allAdmins = await userMethods.readMany(eq(user.role, 'admin'));
+
+// userMethods.updateMany() is also available for batch updates
+await userMethods.updateOne(newUser.id, { role: 'admin' });
+
+// userMethods.deleteMany() is also available for batch deletes
+await userMethods.deleteOne(newUser.id);
 ```
---
+++ packages/drizzle-crud/src/pg-core.ts
@@ -0,0 +1,140 @@
+import {
+  withCursorPagination,
+  withLimitOffsetPagination,
+} from '@antribute/drizzle-pagination/pg-core';
+import { eq, inArray, sql } from 'drizzle-orm';
+import type { GetColumnData, SQL } from 'drizzle-orm';
+import type { PgDatabase, PgTable } from 'drizzle-orm/pg-core';
+
+import type { ColumnOfTable, PaginatedData, PaginationMethods, PaginationParams } from './shared';
+
+export const createCrudMethods = <
+  Db extends PgDatabase<any>,
+  Table extends PgTable,
+  PrimaryKeyCol extends ColumnOfTable<Table>,
+  PaginationMethod extends PaginationMethods
+>(
+  db: Db,
+  table: Table,
+  primaryKeyCol: PrimaryKeyCol,
+  opts: {
+    paginationMethod: PaginationMethod;
+  }
+) => {
+  type PrimaryKey = GetColumnData<PrimaryKeyCol>;
+  type InsertShape = Table['_']['inferInsert'];
+  type UpdateShape = Partial<InsertShape>;
+  type ReturnShape = Table['_']['inferSelect'];
+
+  const createOne = async (value: InsertShape): Promise<ReturnShape> => {
+    const data = await db.insert(table).values(value).returning();
+    return data[0]!;
+  };
+  const createMany = async (values: InsertShape[]): Promise<ReturnShape[]> => {
+    const data = db.insert(table).values(values).returning();
+
+    return data;
+  };
+
+  const readOne = async (primaryKey: PrimaryKey): Promise<ReturnShape> => {
+    const data = await db.select().from(table).where(eq(primaryKeyCol, primaryKey)).limit(1);
+    if (!data[0]) {
+      throw new Error('Not Found');
+    }
+    return data[0];
+  };
+  const readMany = async (
+    where?: SQL,
+    pagination?: PaginationParams<PaginationMethod, PrimaryKeyCol>
+  ): Promise<PaginatedData<PaginationMethod, ReturnShape, PrimaryKeyCol>> => {
+    const query = db.select().from(table).where(where);
+
+    if (opts.paginationMethod === 'cursor') {
+      const cPagination = pagination as PaginationParams<'cursor', PrimaryKeyCol>;
+      const [rows, count] = await Promise.all([
+        withCursorPagination(
+          query.$dynamic(),
+          primaryKeyCol,
+          cPagination?.cursor,
+          cPagination?.pageSize
+        ),
+        db.select({ count: sql<string>`count(*)` }).from(table),
+      ]);
+      const total = Number(count[0]?.count ?? '0');
+      const data = {
+        data: rows,
+        pagination: {
+          next: rows[rows.length - 1]![primaryKeyCol.name],
+          total,
+        },
+      };
+
+      return data as PaginatedData<PaginationMethod, ReturnShape, PrimaryKeyCol>;
+    }
+
+    if (opts.paginationMethod === 'limit-offset') {
+      const loPagination = pagination as
+        | PaginationParams<'limit-offset', PrimaryKeyCol>
+        | undefined;
+      const [rows, count] = await Promise.all([
+        withLimitOffsetPagination(
+          query.$dynamic(),
+          loPagination?.page ?? null,
+          loPagination?.pageSize ?? undefined
+        ),
+        db.select({ count: sql<string>`count(*)` }).from(table),
+      ]);
+      const total = Number(count[0]?.count ?? '0');
+      const data = {
+        data: rows,
+        pagination: {
+          next:
+            loPagination?.page && loPagination.page * (pagination?.pageSize ?? 10) <= total
+              ? loPagination.page + 1
+              : null,
+          prev: loPagination?.page && loPagination.page > 1 ? loPagination.page - 1 : null,
+          total,
+        },
+      };
+      return data as PaginatedData<PaginationMethod, ReturnShape, PrimaryKeyCol>;
+    }
+
+    const data = await query;
+    if (opts.paginationMethod === 'none') {
+      return data as PaginatedData<PaginationMethod, ReturnShape, PrimaryKeyCol>;
+    }
+    throw new Error('Invalid pagination method');
+  };
+
+  const updateOne = async (primaryKey: PrimaryKey, value: UpdateShape): Promise<ReturnShape> => {
+    const data = await db.update(table).set(value).where(eq(primaryKeyCol, primaryKey)).returning();
+    return data[0]!;
+  };
+  const updateMany = async (
+    primaryKeys: PrimaryKey[],
+    value: UpdateShape
+  ): Promise<ReturnShape[]> => {
+    const data = db.update(table).set(value).where(inArray(primaryKeyCol, primaryKeys)).returning();
+    return data;
+  };
+
+  const deleteOne = async (primaryKey: PrimaryKey): Promise<ReturnShape> => {
+    const data = await db.delete(table).where(eq(primaryKeyCol, primaryKey)).returning();
+    return data[0]!;
+  };
+  const deleteMany = async (primaryKeys: PrimaryKey[]): Promise<ReturnShape[]> => {
+    const data = await db.delete(table).where(inArray(primaryKeyCol, primaryKeys)).returning();
+    return data;
+  };
+
+  return {
+    createMany,
+    createOne,
+    deleteMany,
+    deleteOne,
+    readMany,
+    readOne,
+    updateMany,
+    updateOne,
+  };
+};
